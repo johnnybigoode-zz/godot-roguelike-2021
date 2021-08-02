2358f6bd51980da91669c6cb1068e8bc17b81691
João A. Marson <jamarson@gmail.com>
2021-08-02 19:58:52 -0300

    Pass logic to init world

---
+++ scene/main/InitWorld.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+const Player := preload("res://sprite/PC.tscn")
+
+var _new_GroupName := preload("res://library/GroupName.gd").new()
+var _new_ConvertCoord := preload("res://library/ConvertCoord.gd").new()
+
+
+func _ready() -> void:
+	_create_sprite(Player, _new_GroupName.PC, 1, 1)
+	_create_sprite(Player, _new_GroupName.PC, 2, 2)
+	_create_sprite(Player, _new_GroupName.PC, 3, 3)
+
+
+func _create_sprite(
+    prefab: PackedScene, group: String, x: int, y: int, x_offset: int = 0, y_offset: int = 0
+) -> void:
+	var _new_Sprite := prefab.instance() as Sprite
+	_new_Sprite.position = _new_ConvertCoord.index_to_vector(x + x_offset, y + y_offset)
+	_new_Sprite.add_to_group(group)
+	
+	add_child(_new_Sprite)
