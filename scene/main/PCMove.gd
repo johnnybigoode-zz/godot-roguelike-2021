79503e4e3f8e843509ea19a6c777cd4199ce202f
João A. Marson <jamarson@gmail.com>
2021-08-03 21:49:33 -0300

    Add all directions

--- scene/main/PCMove.gd
+++ scene/main/PCMove.gd
@@ -5,3 +5,9 @@ var _new_InputName := preload("res://library/InputName.gd").new()
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed(_new_InputName.MOVE_LEFT):
         print("move left")
+    if event.is_action_pressed(_new_InputName.MOVE_RIGHT):
+        print("move right")
+    if event.is_action_pressed(_new_InputName.MOVE_UP):
+        print("move up")
+    if event.is_action_pressed(_new_InputName.MOVE_DOWN):
+        print("move down")
